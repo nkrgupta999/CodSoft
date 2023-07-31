8d4cebe367b36973cc6271c4cc756cf968e26816
nkrgupta999 <nitinkumargupta999@outlook.com>
2023-08-01 00:23:01 +0530

    Task 3 : Student Management System

---
+++ studentmain.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+// import java.awt.*;
+
+public class studentmain {
+    public static void main(String[] args) {
+    	studentmanagementsystem st = new studentmanagementsystem();
+    	st.pack();
+        st.setVisible(true);
+        st.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
